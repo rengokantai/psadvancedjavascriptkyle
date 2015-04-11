263beb81b27ddb36ca30b041695099b4ba24b3e8
Hernan Y.Ke <yuriqiao@gmail.com>
2015-04-11 09:52:19 +0800

    Add 0218 and 0302

---
+++ ex0218.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Hernan Y.Ke on 4/11/15.
+ */
+/*
+function foo(){
+    var bar= "bar1";
+    this.baz=baz;
+    this.baz();
+}
+
+function baz(){
+    console.log(this.bar);
+}
+
+
+var bar ="bar2";
+foo();
+    */
+
